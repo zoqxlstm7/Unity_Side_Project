e3645f4571b42ebb102ac13967628af034f23834
Bro <Bro@111percent.net>
2020-10-01 13:25:02 +0900

    발사체 구현

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,25 @@ public class Player : Actor
 {
     #region Variables
     [SerializeField] float moveSpeed = 5.0f;
+    [SerializeField] float attackRange = 5.0f;
+    [SerializeField] float attackSpeed = 0.5f;
+
+    [SerializeField] LayerMask targetMask;
+    [SerializeField] Transform fireTransform = null;
+
+    Transform target = null;
+    float distanceToTarget = 0.0f;
+
+    float startAttackTime = 0.0f;
     #endregion Variables
 
     #region Actor Methods
     public override void UpdateActor()
     {
         UpdateMove();
+        SearchEnemy();
+
+        CheckAttaack();
     }
     #endregion Actor Methods
 
@@ -26,6 +39,46 @@ public class Player : Actor
 
         transform.forward = moveVector;
         transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
+
+    void SearchEnemy()
+    {
+        target = null;
+        distanceToTarget = 0.0f;
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, attackRange, targetMask);
+        if(colliders.Length > 0)
+        {
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i].GetComponent<IDamageable>().IsDead)
+                    continue;
+
+                Transform findTarget = colliders[i].transform;
+
+                float distanceToFindTarget = Vector3.Distance(transform.position, findTarget.position);
+                if (target == null || distanceToTarget > distanceToFindTarget)
+                {
+                    target = findTarget;
+                    distanceToTarget = distanceToFindTarget;
+                }
+            }
+        }
+    }
+
+    void CheckAttaack()
+    {
+        if (target == null)
+            return;
+
+        if(Time.time - startAttackTime > attackSpeed)
+        {
+            string filePath = "Projectile/Projectile";
+            Projectile projectile = InGameSceneManager.instance.ProjectileManager.Generate(filePath, fireTransform.position);
+            projectile.Fire(this, target, targetMask);
+
+            startAttackTime = Time.time;
+        }
     }
     #endregion Helper Methods
 }
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -4,7 +4,79 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    #region Variables
+    [SerializeField] float moveSpeed = 10.0f;
+    [SerializeField] float damage = 3.0f;
+
+    Actor owner = null;
+    Transform target = null;
+    int targetLayer = -1;
+
+    bool isMove = false;
+    bool isReturn = false;
+    #endregion Variables
+
     #region Property
     public string FilePath { get; set; }
     #endregion Property
+
+    #region Unity Methods
+    private void Update()
+    {
+        UpdateMove();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(isReturn)
+        {
+            if(other.gameObject.layer == owner.gameObject.layer)
+            {
+                Remove();
+            }
+        }
+
+        if (other.gameObject.layer == targetLayer)
+        {
+            other.GetComponent<IDamageable>().TakeDamage(damage, null);
+
+            target = owner.transform;
+            isReturn = true;
+        }
+    }
+    #endregion Unity Methods
+
+    #region Helper Methods
+    void UpdateMove()
+    {
+        if (!isMove)
+            return;
+
+        Vector3 dir = (target.position - transform.position).normalized;
+        transform.forward = dir;
+
+        transform.position += dir * moveSpeed * Time.deltaTime;
+    }
+
+    void Remove()
+    {
+        targetLayer = -1;
+
+        isReturn = false;
+        isMove = false;
+
+        InGameSceneManager.instance.ProjectileManager.Remove(FilePath, gameObject);
+    }
+    #endregion Helper Methods
+
+    #region Other Methods
+    public void Fire(Actor owner, Transform target, LayerMask targetMask)
+    {
+        this.owner = owner;
+        this.target = target;
+        targetLayer = (int)Mathf.Log(targetMask.value, 2);
+
+        isMove = true;
+    }
+    #endregion Other Methods
 }
