b83dc72a29563a1c2418c25b4f35607cbc30c600
Bro <Bro@111percent.net>
2020-10-02 02:04:20 +0900

    맵 제너레이터 추가

---
+++ Assets/Editor/MapGenerator_SceneEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGenerator_SceneEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        MapGenerator mapGenerator = target as MapGenerator;
+        mapGenerator.GenerateMap();
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    #region Variables
+    [SerializeField] Transform tilePrefab = null;
+    [SerializeField] Vector2 mapSize = Vector2.zero;
+
+    [Range(0, 1)]
+    [SerializeField] float outlinePercent = 0.0f;
+    #endregion Variables
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+
+    public void GenerateMap()
+    {
+        string holderName = "Generated Map";
+        if (transform.Find(holderName))
+        {
+            DestroyImmediate(transform.Find(holderName).gameObject);
+        }
+
+        Transform mapHolder = new GameObject(holderName).transform;
+        mapHolder.transform.SetParent(transform);
+
+        for (int x = 0; x < mapSize.x; x++)
+        {
+            for (int y = 0; y < mapSize.y; y++)
+            {
+                Vector3 tilePosition = new Vector3(-mapSize.x / 2 + 0.5f + x, 0.0f, -mapSize.y / 2 + 0.5f + y);
+                Transform newTile = Instantiate(tilePrefab, tilePosition, Quaternion.Euler(Vector3.right * 90.0f)) as Transform;
+                newTile.localScale = Vector3.one * (1 - outlinePercent);
+                newTile.SetParent(mapHolder);
+            }
+        }
+    }
+}
